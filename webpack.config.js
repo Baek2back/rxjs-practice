55b903bc87a0b1a131417eff1d554c98b29ecbef
Baek2back <dumbuk12@naver.com>
2021-07-29 22:07:14 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src/")],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: "css-loader",
+            options: { import: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    hot: false,
+    historyApiFallback: true,
+    liveReload: true,
+    open: true,
+    port: 5500,
+    watchContentBase: true,
+    watchOptions: {
+      poll: 1000,
+      ignored: /node_modules/,
+    },
+  },
+};
